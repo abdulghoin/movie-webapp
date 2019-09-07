ed95a920d78bac209752331bc56499dda9e3b489
Abdul Ghofur <abdulghoin@gmail.com>
2019-09-07 13:51:43 +0700

    ignore .env file & import to config

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -3,15 +3,20 @@ import {MdSearch} from 'react-icons/md';
 
 import './SearchMovie.css';
 
-const SearchMovie = () => (
-  <form className="search-movie">
-    <input placeholder='movie name' />
-    <div>
-      <button type="submit">
-        <MdSearch />
-      </button>
-    </div>
-  </form>
-);
+import {API} from '../config';
+
+const SearchMovie = () => {
+	console.log(API)
+  return (
+    <form className="search-movie">
+      <input placeholder="movie name" />
+      <div>
+        <button type="submit">
+          <MdSearch />
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default SearchMovie;
